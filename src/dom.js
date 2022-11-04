9e903d4d67f5890faf67c42cdd0f1081ccca3c27
luksvrd <ljvy9b@gmail.com>
2022-11-04 15:26:58 -0500

    Add const to export

---
+++ src/dom.js
@@ -0,0 +1,16 @@
+export const form = document.querySelector("form");
+export const highScoresUL = document.querySelector("#high-scores")
+export const introP = document.querySelector("#intro");
+
+export const quiz = document.getElementById("quiz");
+export const quizP = quiz?.querySelector("#quiz p");
+export const quizH2 = quiz?.querySelector("#quiz h2");
+export const quizList = quiz?.querySelector("#quiz ul");
+
+export const restBtn = document.querySelector("#button[type='reset']");
+
+export const secondsP = document.getElementById("seconds");
+export const startButton = document.getElementById("start-btn");
+
+export const results = document.getElementById("results");
+export const resultsSpan = results?.querySelector("#results span")
