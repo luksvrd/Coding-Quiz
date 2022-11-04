78804535121ee6680c925e4aa10810c4ac661bab
luksvrd <ljvy9b@gmail.com>
2022-11-04 15:28:51 -0500

    Add frame for Qustions & scores

---
+++ src/lib.js
@@ -0,0 +1,19 @@
+// list of Questions, Choices, and Answers
+export const QUESTIONS = [
+  {
+    title: "Commonly used data types DO NOY include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {},
+  {},
+  {},
+];
+
+export const getSortedScores = () => {
+
+};
+
+export const updateScores = (initials, newScore) => {
+
+}
