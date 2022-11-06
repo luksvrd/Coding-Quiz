47c1f6dddb6bb53ad6f419b66830b42af055768e
luksvrd <ljvy9b@gmail.com>
2022-11-06 16:05:18 -0600

    formatting

--- src/lib.js
+++ src/lib.js
@@ -5,11 +5,14 @@ export const QUESTIONS = [
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts",
   },
+
   {
   title: "The condition in an if / else statement is enclosed within _____.",
   choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
   answer: "parentheses",},
-  {title: "Arrays in JavaScript can be used to store _____.",
+
+  {
+    title: "Arrays in JavaScript can be used to store _____.",
   choices: ["numbers & strings", "other arrays", "booleans", "all of the above"],
   answer: "all of the above",},
   {},
