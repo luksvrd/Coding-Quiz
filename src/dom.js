ddf1420089ced8241f3ad045666f3d9493945bac
luksvrd <ljvy9b@gmail.com>
2022-11-06 16:04:58 -0600

    Add choiceButton variable

--- src/dom.js
+++ src/dom.js
@@ -6,6 +6,7 @@ export const quiz = document.getElementById("quiz");
 export const quizP = quiz?.querySelector("#quiz p");
 export const quizH2 = quiz?.querySelector("#quiz h2");
 export const quizList = quiz?.querySelector("#quiz ul");
+export const choiceButton = quiz?.querySelector("choice-button")
 
 export const resetBtn = document.querySelector("#button [type='reset']");
 
